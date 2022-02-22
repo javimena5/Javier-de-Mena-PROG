c30b097394fdcaae40b0f0929d9d491914dcf3cf
Javier de Mena <47533298+javimena5@users.noreply.github.com>
2022-02-22 10:46:09 +0100

    Empezado ejercicio 3

---
+++ Ficheros (Path)/Ejercicio 3/creaDirectorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace creaDirectorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ficheros (Path)/Ejercicio 3/eliminaDirectorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eliminaDirectorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
