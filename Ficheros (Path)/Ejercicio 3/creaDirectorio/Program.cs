8464be216f4b79dad02058e76cb2d6f009268a0d
Javier de Mena <47533298+javimena5@users.noreply.github.com>
2022-02-23 19:38:13 +0100

    creaDirectorio acabado

--- Ficheros (Path)/Ejercicio 3/creaDirectorio/Program.cs
+++ Ficheros (Path)/Ejercicio 3/creaDirectorio/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+// Javier de Mena Asenjo
+using System;
 using System.IO;
 
 namespace creaDirectorio
@@ -8,13 +9,20 @@ namespace creaDirectorio
         static void Main(string[] args)
         {
             if(args.Length == 1){
-                if(!Directory.Exists(args[0]))
+                if(!Directory.Exists(args[0])){
                     Directory.CreateDirectory(args[0]);
+                    Console.Write("Directorio creado.");
+                }
+                else {
+                    Console.Write(" El directorio introducido ya existe en el sistema.");
+                }
             }else {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.Write("ERROR. Argumentos incorrectos. [ creaDirectorio <ruta> ]");
+                Console.ResetColor();
             }
-            Console.ReadKey();
         }
     }
 }
+
+// Javier de Mena Asenjo
