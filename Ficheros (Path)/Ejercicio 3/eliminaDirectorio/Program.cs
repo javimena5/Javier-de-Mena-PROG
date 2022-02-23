188967ada9bb1b47d19929b37f70d09e5d1545ce
Javier de Mena <47533298+javimena5@users.noreply.github.com>
2022-02-23 20:26:42 +0100

    eliminaDirectorio acabado

--- Ficheros (Path)/Ejercicio 3/eliminaDirectorio/Program.cs
+++ Ficheros (Path)/Ejercicio 3/eliminaDirectorio/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+// Javier de Mena Asenjo
+using System;
+using System.IO;
 
-namespace eliminaDirectorio
+namespace eliminaFichero
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length == 1){
+                if(File.Exists(args[0])){
+                    File.Delete(args[0]);
+                    Console.Write(" Directorio eliminado.");
+                }
+                else {
+                    Console.Write(" El directorio introducido no existe.");
+                }
+            }else {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(" ERROR. Argumentos incorrectos. [ eliminaFichero <ruta> ]");
+                Console.ResetColor();
+            }
         }
     }
 }
+// Javier de Mena Asenjo
