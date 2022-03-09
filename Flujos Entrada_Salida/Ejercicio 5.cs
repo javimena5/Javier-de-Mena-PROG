819d4bbf04b05f108b12ed5bba2481610879a79c
Javier de Mena <47533298+javimena5@users.noreply.github.com>
2022-03-09 13:30:19 +0100

    Avances ejercicio 5 flujo datos

---
+++ Flujos Entrada_Salida/Ejercicio 5.cs
@@ -0,0 +1,18 @@
+// Javier de Mena Asenjo
+using System;
+using System.Text.RegularExpressions;
+
+namespace Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
+
+
+// Javier de Mena Asenjo
