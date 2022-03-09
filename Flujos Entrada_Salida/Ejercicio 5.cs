27f15fa253e81ec3f8a01056016e14a1398cc0a3
Javier de Mena <47533298+javimena5@users.noreply.github.com>
2022-03-09 14:07:01 +0100

    Mas avances ejercicio 5

--- Flujos Entrada_Salida/Ejercicio 5.cs
+++ Flujos Entrada_Salida/Ejercicio 5.cs
@@ -1,6 +1,6 @@
 // Javier de Mena Asenjo
 using System;
-using System.Text.RegularExpressions;
+using System.IO;
 
 namespace Prueba
 {
@@ -8,8 +8,25 @@ namespace Prueba
     {
         static void Main(string[] args)
         {
-            
+            Path ruta = @"C:/datos/datos.txt";
+            if(!Directory.Exists(ruta)) Directory.CreateDirectory(ruta);
+            FileStream fs = new FileStream("datos.txt",FileMode.Create,FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs, Encoding.UTF8);
+            ConsoleKeyInfo cki = null;
+            do{
+                cki = Console.ReadKey();
+                bw.Write(cki.KeyChar);
+            }while(cki.KeyChar != ConsoleKey.Escape);
             Console.ReadKey();
+
+            
+            using (FileStream stream = new FileStream(rutaFichero, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (BinaryWriter streamRB = new BinaryReader(stream))
+            {
+                
+            }
+
+
         }
     }
 }
